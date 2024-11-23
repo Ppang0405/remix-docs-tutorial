435854046cbb06d54f34ea3ee3deab96f0e184cb
Jeremy <jeremyoppa012@gmail.com>
2024-11-23 16:19:04 +0700

    finish tutorial

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,31 +1,37 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { Form, json, useLoaderData } from "@remix-run/react";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, json, useFetcher, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 import invariant from "tiny-invariant";
 
-export const loader = async ({
-    params,
-  }: LoaderFunctionArgs) => {
-    invariant(params.contactId, "Missing contactId param");
-    const contact = await getContact(params.contactId);
-    if (!contact) {
-        throw new Response("Not Found", { status: 404 });
-      }
-    return json({ contact });
-  };
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact });
+};
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+};
 
 export default function Contact() {
-//   const contact = {
-//     first: "Your",
-//     last: "Name",
-//     avatar: "https://placecats.com/200/200",
-//     twitter: "your_handle",
-//     notes: "Some notes",
-//     favorite: true,
-//   };
-const { contact } = useLoaderData<typeof loader>();
+  //   const contact = {
+  //     first: "Your",
+  //     last: "Name",
+  //     avatar: "https://placecats.com/200/200",
+  //     twitter: "your_handle",
+  //     notes: "Some notes",
+  //     favorite: true,
+  //   };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
@@ -51,9 +57,7 @@ const { contact } = useLoaderData<typeof loader>();
 
         {contact.twitter ? (
           <p>
-            <a
-              href={`https://twitter.com/${contact.twitter}`}
-            >
+            <a href={`https://twitter.com/${contact.twitter}`}>
               {contact.twitter}
             </a>
           </p>
@@ -89,21 +93,20 @@ const { contact } = useLoaderData<typeof loader>();
 const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
 
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
-        aria-label={
-          favorite
-            ? "Remove from favorites"
-            : "Add to favorites"
-        }
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
         name="favorite"
         value={favorite ? "false" : "true"}
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 };
